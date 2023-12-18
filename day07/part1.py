8f8a1788c26e803688e4a10b05c35a7fabf43b3b
ian contijoch <ian.contijoch@gmail.com>
2023-12-18 00:42:59 -0500

    Day 07

---
+++ day07/part1.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+from collections import Counter
+from collections import namedtuple
+from enum import auto
+from enum import Enum
+from typing import Iterator
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+class HandType(Enum):
+    HIGH_CARD = auto()
+    ONE_PAIR = auto()
+    TWO_PAIR = auto()
+    THREE_OF_A_KIND = auto()
+    FULL_HOUSE = auto()
+    FOUR_OF_A_KIND = auto()
+    FIVE_OF_A_KIND = auto()
+
+
+CARD_VALS = {
+    'T': 10,
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14,
+}
+
+
+Hand = namedtuple('Hand', ('value', 'type', 'bid'))
+
+
+def find_type(hand: str) -> HandType:
+    c = Counter(hand)
+    pairs = {card for card, count in c.most_common() if count == 2}
+    threes = {card for card, count in c.most_common() if count == 3}
+    fours = {card for card, count in c.most_common() if count == 4}
+    fives = {card for card, count in c.most_common() if count == 5}
+
+    if len(c.values()) == 5:
+        return HandType.HIGH_CARD
+    elif len(pairs) == 1 and not threes:
+        return HandType.ONE_PAIR
+    elif len(pairs) == 2:
+        return HandType.TWO_PAIR
+    elif threes and not pairs:
+        return HandType.THREE_OF_A_KIND
+    elif threes and pairs:
+        return HandType.FULL_HOUSE
+    elif fours:
+        return HandType.FOUR_OF_A_KIND
+    elif fives:
+        return HandType.FIVE_OF_A_KIND
+    else:
+        raise NotImplementedError(f'could not identify hand type: {hand}')
+
+
+def rank_ties(tied_hands: Iterator[Hand]) -> list[Hand]:
+    tie_values = {
+        h: tuple(
+            map(
+                int, (
+                    CARD_VALS.get(c, c)
+                    for c in h.value
+                ),
+            ),
+        ) for h in tied_hands
+    }
+    return sorted(tie_values, key=lambda x: tie_values.get(x) or -1)
+
+
+def compute(s: str) -> int:
+    hands = set()
+    lines = s.splitlines()
+    for line in lines:
+        hand, bid = line.split()
+        hands.add(Hand(hand, find_type(hand), int(bid)))
+
+    ranked_hands = []
+    for x in range(1, len(HandType) + 1):
+        compare_lst = (h for h in hands if h.type.value == x)
+        ranked_hands.extend(rank_ties(compare_lst))
+
+    return sum(x * (i+1) for i, x in enumerate(r.bid for r in ranked_hands))
+
+
+INPUT_S = '''\
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+'''
+EXPECTED = 6440
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
