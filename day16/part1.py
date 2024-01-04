9ce49f9ea4fa6060db60125edbcae85c96de17d9
ian contijoch <ian.contijoch@gmail.com>
2024-01-04 00:44:37 -0500

    day 16

---
+++ day16/part1.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+from support import Direction4
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def move(
+    d: Direction4, x: int, y: int,
+    coords: dict[tuple[int, int], str],
+    visited: set[tuple[Direction4, int, int]] | None = None,
+) -> set[tuple[Direction4, int, int]]:
+
+    if visited is None:
+        visited = set()
+
+    x, y = d.apply(x, y)
+    if (x, y) not in coords or (d, x, y) in visited:
+        return visited
+    c = coords[(x, y)]
+    visited.add((d, x, y))
+    if c == '.':
+        visited = move(d, x, y, coords, visited)
+    elif c == '|':
+        if d in (Direction4.DOWN, Direction4.UP):
+            visited = move(d, x, y, coords, visited)
+        else:
+            visited = move(Direction4.UP, x, y, coords, visited)
+            visited = move(Direction4.DOWN, x, y, coords, visited)
+    elif c == '-':
+        if d in (Direction4.LEFT, Direction4.RIGHT):
+            visited = move(d, x, y, coords, visited)
+        else:
+            visited = move(Direction4.LEFT, x, y, coords, visited)
+            visited = move(Direction4.RIGHT, x, y, coords, visited)
+    elif c == '\\':
+        if d is Direction4.UP:
+            visited = move(Direction4.LEFT, x, y, coords, visited)
+        elif d is Direction4.RIGHT:
+            visited = move(Direction4.DOWN, x, y, coords, visited)
+        elif d is Direction4.LEFT:
+            visited = move(Direction4.UP, x, y, coords, visited)
+        elif d is Direction4.DOWN:
+            visited = move(Direction4.RIGHT, x, y, coords, visited)
+        else:
+            raise NotImplementedError
+    elif c == '/':
+        if d is Direction4.UP:
+            visited = move(Direction4.RIGHT, x, y, coords, visited)
+        elif d is Direction4.RIGHT:
+            visited = move(Direction4.UP, x, y, coords, visited)
+        elif d is Direction4.LEFT:
+            visited = move(Direction4.DOWN, x, y, coords, visited)
+        elif d is Direction4.DOWN:
+            visited = move(Direction4.LEFT, x, y, coords, visited)
+        else:
+            raise NotImplementedError
+    else:
+        raise RuntimeError
+    return visited
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    lines = lines[1:] if not lines[0] else lines
+    coords = {
+        (x, y): c
+        for y, line in enumerate(lines)
+        for x, c in enumerate(line)
+    }
+    start = (Direction4.RIGHT, -1, 0)
+    visited = move(*start, coords)
+
+    return len({(x, y) for _, x, y in visited})
+
+
+INPUT_S = r"""
+.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....
+"""
+EXPECTED = 46
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
