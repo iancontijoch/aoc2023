b47add7b580b616911b5cc6bf440b795c10f802b
ian contijoch <ian.contijoch@gmail.com>
2024-01-20 20:30:02 -0500

    day 25

---
+++ day25/part1.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import argparse
+import math
+import os.path
+
+import networkx as nx
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    G = nx.Graph()
+    for line in lines:
+        node_s, to_s = line.split(': ')
+        neighbors = to_s.split(' ')
+        G.add_edges_from([(node_s, n) for n in neighbors])
+
+    G.remove_edges_from(nx.minimum_edge_cut(G))
+    return math.prod(len(c) for c in nx.connected_components(G))
+
+
+INPUT_S = '''\
+jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr
+'''
+EXPECTED = 54
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
